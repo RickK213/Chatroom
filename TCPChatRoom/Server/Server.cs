d674af41fc9f86c960474b071854863ea98786d6
RickK213 <rkippert@att.net>
2017-12-14 10:08:02 -0600

    Commented out all code within the Run method on the server class

--- TCPChatRoom/Server/Server.cs
+++ TCPChatRoom/Server/Server.cs
@@ -40,14 +40,44 @@ namespace Server
 
         public void Run()
         {
-            AcceptClient();
-            string message = client.Recieve();
-            Respond(message);
-        }
-        private void AcceptClient()
+            //Use Parallel.Invode to allow for multiple connections to the server
+            //Original Code: msdn.microsoft.com/en-us/library/dd992634(v=vs.110).aspx
+        //    while (true)
+        //    {
+        //        try
+        //        {
+        //            Parallel.Invoke(AcceptClient,    // Param #0 - static method
+        //                () =>           // Param #1 - lambda expression
+        //            {
+        //                    Console.WriteLine("Method=beta, Thread={0}", Thread.CurrentThread.ManagedThreadId);
+        //                },
+        //                delegate ()     // Param #2 - in-line delegate
+        //            {
+        //                    Console.WriteLine("Method=gamma, Thread={0}", Thread.CurrentThread.ManagedThreadId);
+        //                }
+        //            );
+        //        }
+        //        // No exception is expected in this example, but if one is still thrown from a task,
+        //        // it will be wrapped in AggregateException and propagated to the main thread.
+        //        catch (AggregateException e)
+        //        {
+        //            Console.WriteLine("An action has thrown an exception. THIS WAS UNEXPECTED.\n{0}", e.InnerException.ToString());
+        //        }
+
+
+
+
+
+
+        //    }
+        //    string message = client.Recieve();
+        //    Respond(message);
+        //}
+
+        public void AcceptClient()
         {
             TcpClient clientSocket = default(TcpClient);
-            clientSocket = server.AcceptTcpClient();
+            clientSocket = server.AcceptTcpClient(); //this is blocking
             Console.WriteLine("Connected");
             NetworkStream stream = clientSocket.GetStream();
             client = new User(stream, clientSocket);
