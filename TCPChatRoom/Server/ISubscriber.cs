f694cf98a7c46a62caa91eeb510dd232b78132ea
David Perez <d.perez89@aol.com>
2017-12-15 09:54:18 -0600

    implementing the observer design pattern for users and user list. Adding in the Isubscriber interface.

---
+++ TCPChatRoom/Server/ISubscriber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    interface ISubscriber
+    {
+        void Notify(User newUser);
+        void Send(Message message);
+        Message Recieve();
+    }
+}
